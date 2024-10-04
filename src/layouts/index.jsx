40c7d15c87824c98fc3d1b824cb56507fa8ca691
George <george.nabil.biy@gmail.com>
2024-10-04 22:04:39 +0300

    Layout

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,10 +1,20 @@
 // React
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 // UI
-import { AppBreadcrumb, Jumbotron, Button } from "/src";
+import { AppBreadcrumb, Jumbotron, AppButton } from "/src";
+
+// Components
+import { AppHeader } from "./header";
+import { AppSidebar } from "./sidebar";
+import { AppFooter } from "./footer";
 
 export const MainLayout = () => {
+  // State
+  const [isSidebarVisibile, setSidebarVisibility] = useState(true);
+
+  // @TODO: Static data
   const data = [
     { path: "/", label: "Home" },
     { path: "/sayed", label: "Sayed", outline: true },
@@ -15,15 +25,33 @@ export const MainLayout = () => {
 
   return (
     <div className="p-2">
-      <AppBreadcrumb />
-      <Jumbotron />
-      <div className="flex flex-wrap gap-2 p-2">
-        {data.map(({ label, path, ...button }) => (
-          <Button key={path} label={label} path={path} {...button} />
-        ))}
+      <AppHeader
+        isSidebarVisibile={isSidebarVisibile}
+        setSidebarVisibility={setSidebarVisibility}
+      />
+
+      <AppSidebar
+        isSidebarVisibile={isSidebarVisibile}
+        setSidebarVisibility={setSidebarVisibility}
+      />
+
+      <div className="mt-14 mb-20 space-y-4">
+        <AppBreadcrumb />
+
+        <Jumbotron />
+
+        <div className="p-4 border-2 rounded-2xl">
+          <div className="flex flex-wrap gap-2 p-2">
+            {data.map(({ label, path, ...button }) => (
+              <AppButton key={path} label={label} path={path} {...button} />
+            ))}
+          </div>
+
+          <Outlet />
+        </div>
       </div>
 
-      <Outlet />
+      <AppFooter />
     </div>
   );
 };
