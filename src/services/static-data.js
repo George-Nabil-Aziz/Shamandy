8b2c7c921d247bb56e66af50db53eb72f49f819b
George <george.nabil.biy@gmail.com>
2024-10-05 13:38:52 +0300

    Static data

--- src/services/static-data.js
+++ src/services/static-data.js
@@ -1,19 +1,12 @@
 // React icons
-import {
-  HiChartPie,
-  HiInbox,
-  HiShoppingBag,
-  HiTable,
-  HiUser,
-} from "react-icons/hi";
+import { HiChartPie, HiShoppingBag, HiTable, HiUser } from "react-icons/hi";
 
 export const StaticData = () => {
   // TODO: Static data
   const data = [
     { path: "/", label: "Home", icon: HiChartPie },
-    { path: "/sayed", label: "Sayed", icon: HiInbox },
-    { path: "/hello", label: "Hello", icon: HiUser },
-    { path: "/hello/beeb", label: "History", icon: HiShoppingBag },
+    { path: "/sayed", label: "Sayed", icon: HiUser },
+    { path: "/history", label: "History", icon: HiShoppingBag },
     { path: "/page", label: "#", icon: HiTable },
   ];
   return { data };
