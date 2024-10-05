f1c6d3b86603209663e09e3904b5045a51f5c82e
George <george.nabil.biy@gmail.com>
2024-10-05 13:52:23 +0300

    404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,7 +4,7 @@ import { AppButton } from "/src";
 export const Error404Page = () => {
   return (
     <div className="h-screen bg-[url('/donkey.png')] bg-no-repeat bg-center bg-gray-500">
-      <AppButton label="Go home you donkey" path="/" danger />
+      <AppButton label="Go home" path="/" danger />
     </div>
   );
 };
