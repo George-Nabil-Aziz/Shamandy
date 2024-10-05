519ac8c6e98a1e0812ec41c82004ae1f795e3dd1
George <george.nabil.biy@gmail.com>
2024-10-05 14:29:25 +0300

    Enhancement

--- src/services/static-data.js
+++ src/services/static-data.js
@@ -1,13 +1,14 @@
 // React icons
-import { HiChartPie, HiShoppingBag, HiUser } from "react-icons/hi";
+import { HiChartPie, HiUser } from "react-icons/hi";
 import { GiDonkey } from "react-icons/gi";
+import { FaHistory } from "react-icons/fa";
 
 export const StaticData = () => {
   // TODO: Static data
   const data = [
     { path: "/", label: "Home", icon: HiChartPie },
     { path: "/sayed", label: "Sayed", icon: HiUser },
-    { path: "/history", label: "History", icon: HiShoppingBag },
+    { path: "/history", label: "History", icon: FaHistory },
     { path: "/donkey", label: "#", icon: GiDonkey },
   ];
   return { data };
