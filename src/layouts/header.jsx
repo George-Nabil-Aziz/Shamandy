df14af1c566548f4a96c3cbae809a4ed1b4a8bc7
George <george.nabil.biy@gmail.com>
2024-10-05 12:55:42 +0300

    Header

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -1,59 +1,69 @@
+// React
+import { Link } from "react-router-dom";
+
 // UI
-import { AppButton } from "/src";
+import { AppButton, StaticData } from "/src";
 
 // Flowbite
 import { DarkThemeToggle, Avatar, Dropdown, Navbar } from "flowbite-react";
 
-export const AppHeader = ({ isSidebarVisibile, setSidebarVisibility }) => (
-  <Navbar fluid rounded className="fixed top-0 right-0 left-0 z-[10] border-b">
-    <Navbar.Brand href="#">
-      {/* <AppButton
-        outline
-        icon="iconamoon:menu-burger-horizontal-fill"
-        onClick={() => setSidebarVisibility(true)}
-      /> */}
-      <img src="/favicon.png" className="mr-3 h-6 sm:h-9" alt="Ta3mia" />
-      <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-        Bashandy
-      </span>
-    </Navbar.Brand>
-    <div className="flex md:order-2">
-      <Dropdown
-        arrowIcon={false}
-        inline
-        label={
-          <Avatar
-            alt="User settings"
-            img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-            rounded
-          />
-        }
-      >
-        <Dropdown.Header>
-          <span className="block text-sm">Bonnie Green</span>
-          <span className="block truncate text-sm font-medium">
-            name@flowbite.com
-          </span>
-        </Dropdown.Header>
-        <Dropdown.Item>Dashboard</Dropdown.Item>
-        <Dropdown.Item>Settings</Dropdown.Item>
-        <Dropdown.Item>Earnings</Dropdown.Item>
-        <Dropdown.Divider />
-        <Dropdown.Item>Sign out</Dropdown.Item>
-      </Dropdown>
+export const AppHeader = ({ isSidebarVisibile, setSidebarVisibility }) => {
+  // TODO: Static data
+  const { data } = StaticData();
 
-      <DarkThemeToggle />
+  return (
+    <Navbar
+      fluid
+      rounded
+      className="fixed top-0 right-0 left-0 z-[10] border-b"
+    >
+      <Navbar.Brand as={Link} to="/">
+        <AppButton
+          outline
+          icon="iconamoon:menu-burger-horizontal-fill"
+          onClick={() => setSidebarVisibility((prev) => !prev)}
+        />
+        <img src="/favicon.png" className="mx-3 h-6 sm:h-9" alt="Ta3mia" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Bashandy
+        </span>
+      </Navbar.Brand>
+      <div className="flex md:order-2 gap-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar
+              alt="User settings"
+              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+              rounded
+            />
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">
+              name@flowbite.com
+            </span>
+          </Dropdown.Header>
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
 
-      <Navbar.Toggle />
-    </div>
-    <Navbar.Collapse>
-      <Navbar.Link href="#" active>
-        Home
-      </Navbar.Link>
-      <Navbar.Link href="#">About</Navbar.Link>
-      <Navbar.Link href="#">Services</Navbar.Link>
-      <Navbar.Link href="#">Pricing</Navbar.Link>
-      <Navbar.Link href="#">Contact</Navbar.Link>
-    </Navbar.Collapse>
-  </Navbar>
-);
+        <DarkThemeToggle />
+
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        {data.map(({ label, path, icon, ...button }) => (
+          <Navbar.Link key={path} as={Link} to={path}>
+            {label}
+          </Navbar.Link>
+        ))}
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
