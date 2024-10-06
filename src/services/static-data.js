0393d9d95bf65e85b41f705fb5f9084c45e29f20
George <george.nabil.biy@gmail.com>
2024-10-06 22:02:30 +0300

    Static data

--- src/services/static-data.js
+++ src/services/static-data.js
@@ -23,3 +23,19 @@ export const PreferencesUser = [
   { name: "Smsm", fool: 2, ta3mia: 3, batates: 7 },
   { name: "Gogo", fool: 2, ta3mia: 3, batates: 7 },
 ];
+
+export const PreferencesUser2 = {
+  Pappy: { fool: 2, ta3mia: 0, batates: 2 },
+  Mummy: { fool: 0, ta3mia: 2, batates: 2 },
+  Pero: { fool: 1, ta3mia: 1, batates: 2 },
+  Devo: { fool: 0, ta3mia: 2, batates: 2 },
+  BoBool: { fool: 2, ta3mia: 1, batates: 0 },
+  Smsm: { fool: 0, ta3mia: 2, batates: 0 },
+  Gogo: { fool: 2, ta3mia: 1, batates: 0 },
+};
+
+export const UnitPrice = {
+  fool: 8,
+  ta3mia: 8,
+  batates: 12,
+};
