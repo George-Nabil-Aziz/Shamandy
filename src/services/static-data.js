a8d485663cb91c941068664ff3d86b92a2cdf631
George <george.nabil.biy@gmail.com>
2024-11-26 21:36:51 +0200

    Add total price

--- src/services/static-data.js
+++ src/services/static-data.js
@@ -15,28 +15,62 @@ export const StaticData = () => {
   return { data };
 };
 
-export const PreferencesUser = [
-  { name: "Pero", fool: 2, ta3mia: 3, batates: 7 },
-  { name: "Devo", fool: 2, ta3mia: 3, batates: 7 },
-  { name: "Mummy", fool: 2, ta3mia: 3, batates: 7 },
-  { name: "Pappy", fool: 2, ta3mia: 3, batates: 7 },
-  { name: "BoBool", fool: 2, ta3mia: 3, batates: 7 },
-  { name: "Smsm", fool: 2, ta3mia: 3, batates: 7 },
-  { name: "Gogo", fool: 2, ta3mia: 3, batates: 7 },
-];
-
-export const PreferencesUser2 = {
-  Pappy: { fool: 2, ta3mia: 0, batates: 2 },
-  Mummy: { fool: 0, ta3mia: 2, batates: 2 },
-  Pero: { fool: 1, ta3mia: 1, batates: 2 },
-  Devo: { fool: 0, ta3mia: 2, batates: 2 },
-  BoBool: { fool: 2, ta3mia: 1, batates: 0 },
-  Smsm: { fool: 0, ta3mia: 2, batates: 0 },
-  Gogo: { fool: 2, ta3mia: 1, batates: 0 },
+export const PreferencesUser = {
+  pappy: {
+    fool: 0,
+    ta3mia: 0,
+    batates: 0,
+    koshary: 0,
+    tagen: 0,
+  },
+  mummy: {
+    fool: 0,
+    ta3mia: 0,
+    batates: 0,
+    koshary: 0,
+    tagen: 0,
+  },
+  batbat: {
+    fool: 0,
+    ta3mia: 0,
+    batates: 0,
+    koshary: 0,
+    tagen: 0,
+  },
+  davdav: {
+    fool: 0,
+    ta3mia: 0,
+    batates: 0,
+    koshary: 0,
+    tagen: 0,
+  },
+  davdav: {
+    fool: 0,
+    ta3mia: 0,
+    batates: 0,
+    koshary: 0,
+    tagen: 0,
+  },
+  smsm: {
+    fool: 0,
+    ta3mia: 0,
+    batates: 0,
+    koshary: 0,
+    tagen: 0,
+  },
+  gogo: {
+    fool: 0,
+    ta3mia: 0,
+    batates: 0,
+    koshary: 0,
+    tagen: 0,
+  },
 };
 
 export const UnitPrice = {
   fool: 8,
   ta3mia: 8,
   batates: 12,
+  koshary: 25,
+  tagen: 0,
 };
