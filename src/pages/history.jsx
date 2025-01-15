c99732a319bcddb4de5e1177c2983c443f00c920
George <george.nabil.biy@gmail.com>
2025-01-16 00:31:31 +0200

    History

--- src/pages/history.jsx
+++ src/pages/history.jsx
@@ -18,11 +18,10 @@ export const History = () => {
       <p>20-11-2024: Koshary, Tagen</p>
       <p>26-11-2024: Add total sandwichs count, total price</p>
       <p>03-12-2024: Netlify 404 Fix</p>
-      <p>03-12-2024: End sprints/1</p>
       <p>27-12-2024: Fixing save localstorage</p>
       <p>27-12-2024: Adding edit module</p>
-      <p>28-12-2024: End sprints/2</p>
-      <p>00-00-2024: Adding firebase cloud server</p>
+      <p>28-12-2024: End sprints/1</p>
+      <p>28-12-2024: Adding firebase cloud server</p>
       <p>00-00-2024: </p>
       <p></p>
 
