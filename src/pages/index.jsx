a4ebd11a707892ce27443a55a2d7f3c788ae6da7
George <george.nabil.biy@gmail.com>
2025-01-16 02:28:32 +0200

    Gad Login

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 export const MainPage = () => (
   <>
     <p>Gad is better than Elprince & Shamandy</p>
-    <img className="w-full" src="/falafel.jpg" alt="Ta3mia" />
+    <img className="w-full" src="/shawerma.jpeg" alt="Ta3mia" />
   </>
 );
