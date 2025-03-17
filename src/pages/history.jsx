06b08cf00c46b10cfde318419604a15d3ff6c749
George <george.nabil.biy@gmail.com>
2025-03-17 10:31:48 +0200

    History

--- src/pages/history.jsx
+++ src/pages/history.jsx
@@ -31,7 +31,7 @@ export const History = () => {
       <p>14-03-2025: Adding admin, user Login</p>
       <p>15-03-2025: Firebase create, edit food functions</p>
       <p>16-03-2025: Firebase fix order</p>
-      <p>16-03-2024: End sprints/3 (Gaad)</p>
+      <p>16-03-2025: End sprints/3 (Gaad)</p>
 
       <hr />
       <h1 className="flex gap-2 items-center">
