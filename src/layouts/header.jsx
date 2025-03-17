ff0500e1ca97b02da040ce9f89c65b8702c82847
George <george.nabil.biy@gmail.com>
2025-03-17 20:32:48 +0200

    Logo and Images

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -49,7 +49,7 @@ export const AppHeader = ({ isSidebarVisibile, setSidebarVisibility }) => {
           className="hidden lg:block"
         />
         <Navbar.Brand as={Link} to="/">
-          <img src="/favicon.png" className="mr-3 h-6 sm:h-9" alt="Ta3mia" />
+          <img src="/medicine.png" className="mr-3 h-6 sm:h-9" alt="Medicine" />
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
             Eltabei™
           </span>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 export const MainPage = () => (
   <>
     <p>Eltabei is worse than Gad & Elprince & Shamandy</p>
-    <img className="w-full" src="/shawerma-eltabei.jpg" alt="Shawerma" />
+    <img className="w-full" src="/fool-eltabei.jpg" alt="Fool" />
   </>
 );
